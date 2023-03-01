c3e2c41f8a4bb808afa2e1c51170419170d92d2c
Tayeb-shahbakhsh <shahbakhshtayyeb@gmail.com>
2023-03-01 15:19:15 +0330

    view pager added to main activity

--- app/src/main/java/com/example/price/MainActivity.kt
+++ app/src/main/java/com/example/price/MainActivity.kt
@@ -2,7 +2,9 @@ package com.example.price
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.TableLayout
+import androidx.viewpager2.widget.ViewPager2
 import com.example.price.databinding.ActivityMainBinding
 import com.google.android.material.tabs.TabLayout
 
@@ -16,24 +18,23 @@ class MainActivity : AppCompatActivity() {
         val fragmentManager = supportFragmentManager
 
         binding.tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener{
-            override fun onTabSelected(tab: TabLayout.Tab?) {
-                if (tab != null) {
-                    when (tab.position) {
-                        0 -> {
-                            fragmentManager
-                                .beginTransaction().replace(R.id.fragment_container, GoldFragment())
-                                .commit()
-                        }
-                        1 -> {
-                            fragmentManager
-                                .beginTransaction().replace(R.id.fragment_container, DollarFragment())
-                                .commit()
-                        }
-                    }
-                }
-            }
+            override fun onTabSelected(tab: TabLayout.Tab?) {}
             override fun onTabUnselected(tab: TabLayout.Tab?) {}
             override fun onTabReselected(tab: TabLayout.Tab?) {}
         })
+
+        binding.viewPager.apply {
+
+            adapter = ViewPagerAdapter(this@MainActivity)
+
+            registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback(){
+                override fun onPageSelected(position: Int) {
+                    super.onPageSelected(position)
+                    binding.tabLayout.getTabAt(position)?.select()
+                }
+
+            })
+
+        }
     }
 }
