224b6274fd2d85eb0a07931cd01d0a9d0ad68986
Tayeb-shahbakhsh <shahbakhshtayyeb@gmail.com>
2023-03-01 14:38:21 +0330

    tab layout transaction added

--- app/src/main/java/com/example/price/MainActivity.kt
+++ app/src/main/java/com/example/price/MainActivity.kt
@@ -2,10 +2,38 @@ package com.example.price
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TableLayout
+import com.example.price.databinding.ActivityMainBinding
+import com.google.android.material.tabs.TabLayout
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val fragmentManager = supportFragmentManager
+
+        binding.tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener{
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                if (tab != null) {
+                    when (tab.position) {
+                        0 -> {
+                            fragmentManager
+                                .beginTransaction().replace(R.id.fragment_container, GoldFragment())
+                                .commit()
+                        }
+                        1 -> {
+                            fragmentManager
+                                .beginTransaction().replace(R.id.fragment_container, DollarFragment())
+                                .commit()
+                        }
+                    }
+                }
+            }
+            override fun onTabUnselected(tab: TabLayout.Tab?) {}
+            override fun onTabReselected(tab: TabLayout.Tab?) {}
+        })
     }
 }
